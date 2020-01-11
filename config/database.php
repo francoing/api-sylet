c6e8ebff4c4383712fa2485c35abbecc69a28ece
francoing <franco.montti.19@gmail.com>
2020-01-11 10:24:06 -0300

    configurado

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_7ac061b2f837317'),
+            'username' => env('DB_USERNAME', 'bd7b387c7dafce'),
+            'password' => env('DB_PASSWORD', 'ff7d9f8c'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
